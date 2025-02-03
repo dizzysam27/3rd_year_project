a6f1855e0de4526a39340c44293696f99831a2e4
Archie <archiehurman@gmail.com>
2025-02-03 16:52:35 +0000

    LED control working

--- Test Code/LED_Control/LED_Brightness_PWM.py
+++ Test Code/LED_Control/LED_Brightness_PWM.py
@@ -1,54 +1,18 @@
 from gpiozero import PWMLED
 from time import sleep
+import numpy as np
 
-led = PWMLED(18)
+led = PWMLED(18, frequency = 2000)
 
+# led.value = 0.1
+# sleep(5)
+# led.value = 0.5
+# sleep(5)
+# led.value = 1
+# sleep(5)
+# led.value=0
 while True:
-    led.value = 0 
-    sleep(2)
-    led.value = 0.009
-    sleep(2)
-    led.value = 0.01  
-    sleep(2)
-    led.value = 1  
-    sleep(2)
-# from gpiozero.pins.pigpio import PiGPIOFactory
-# myFactory = PiGPIOFactory()
-# from gpiozero import LED
-# LED1 = LED(18,pin_factory=myFactory)
-# LED1.value = -1
-# LED=18 
-# GPIO.setmode( GPIO.BCM )
-# GPIO.setup(LED, GPIO.OUT )
-# pwm=GPIO.PWM(LED,1000)
-
-# pwm.start(5)
-
-# pwm.ChangeDutyCycle(7.5)
-# sleep(2)
-
-# pwm.ChangeDutyCycle(8.5)
-# sleep(2)
-# pwm.ChangeDutyCycle(6.5)
-# sleep(2)
-# pwm.ChangeDutyCycle(7.5)
-# sleep(2)
-# pwm.stop() 
-# while True:
-#     for y in range (0,np.pi,0.01):
-#         test_duty = Max_Duty * np.sin(y)
-#         LED_drive.change_duty_cycle(test_duty)
-#         time.sleep(0.05)
-
-# def LED_control (Duty, Stop):
-#     if Stop == 1:
-#         LED_drive.stop()
-#     else:
-#         LED_drive.start(1)
-#         if Duty >= Max_Duty:
-#             LED_drive.change_duty_cycle(Max_Duty)
-#         else:
-#             LED_drive.change_duty_cycle(Duty)
-            
-# LED_control(10,0)
-
+    for x in range(1,179,1):
+        y = np.sin(np.deg2rad(x))
+        led.value = y   
+        sleep(0.01)
