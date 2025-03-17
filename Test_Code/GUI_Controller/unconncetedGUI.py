3714b5abba04773d86964ee5c3e86e85595915a1
Alex Watson <bv21918@bristol.ac.uk>
2025-03-17 14:09:28 +0000

    Update unconncetedGUI.py

--- Test_Code/GUI_Controller/unconncetedGUI.py
+++ Test_Code/GUI_Controller/unconncetedGUI.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # PyQt5 Imports
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QFrame
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QPlainTextEdit
 
 # Module Imports
 ### TO BE FILLED OUT
@@ -38,23 +38,26 @@ class App(QWidget):
         contentVBox = QVBoxLayout()
         mainGrid.addLayout(contentVBox, 1, 1, 1, 1)
 
-        # Rate Commands horizontal layout
-        rateHBox = QHBoxLayout()
-
         # Camera Feed
         self.imageLabel = QLabel('Image')
         self.imageLabel.setStyleSheet('border: 5px solid black')
         mainGrid.addWidget(self.imageLabel, 1, 0, 1, 1)
 
-        # Title Block
-        self.titleLabel = QLabel('Title')
-        self.titleLabel.setStyleSheet('border: 5px solid black')
-        mainGrid.addWidget(self.titleLabel, 0, 0, 1, 2)
+        # Header
+        headerHBox = QHBoxLayout()
+        mainGrid.addLayout(headerHBox, 0, 0, 1, 2)
+        self.titleLabel = QLabel('Group 15 Maze Game')
+        self.titleLabel.setStyleSheet('border: 5px solid black; font: bold 50px')
+        headerHBox.addWidget(self.titleLabel)
+        self.modeLabel = QLabel('Menu')
+        self.modeLabel.setStyleSheet('border: 5px solid black; font: bold 50px')
+        self.modeLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignCenter)
+        headerHBox.addWidget(self.modeLabel)
 
         # Timer
         self.timerLabel = QLabel('00:00.0')
         self.timerLabel.setAlignment(QtCore.Qt.AlignCenter)
-        self.timerLabel.setMaximumHeight(150)
+        self.timerLabel.setMinimumHeight(150)
         self.timerLabel.setStyleSheet('border: 5px solid black; font: bold 100px')
         contentVBox.addWidget(self.timerLabel)
 
@@ -64,12 +67,20 @@ class App(QWidget):
         button1Box = QVBoxLayout()
         button2Box = QVBoxLayout()
         button3Box = QVBoxLayout()
+        buttonPH = QLabel('')
+        buttonPH.setMinimumHeight(200)
+        buttonPH.setMaximumWidth(1)
+        buttonLabelHBox.addWidget(buttonPH)
         buttonLabelHBox.addLayout(button1Box)
         buttonLabelHBox.addLayout(button2Box)
         buttonLabelHBox.addLayout(button3Box)
+        buttonPH2 = QLabel('')
+        buttonPH2.setMinimumHeight(200)
+        buttonPH2.setMaximumWidth(1)
+        buttonLabelHBox.addWidget(buttonPH2)
         self.buttonLabel1 = QLabel('AI\nSolve')
         self.buttonLabel1.setAlignment(QtCore.Qt.AlignCenter)
-        self.buttonLabel1.setMaximumSize(200, 80)
+        self.buttonLabel1.setMaximumSize(500, 80)
         self.buttonLabel1.setStyleSheet('font: bold 25px')
         button1Box.addWidget(self.buttonLabel1)
         self.button1 = QPushButton()
@@ -78,7 +89,7 @@ class App(QWidget):
         button1Box.addWidget(self.button1)
         self.buttonLabel2 = QLabel('Manual\nSolve')
         self.buttonLabel2.setAlignment(QtCore.Qt.AlignCenter)
-        self.buttonLabel2.setMaximumSize(200, 80)
+        self.buttonLabel2.setMaximumSize(500, 80)
         self.buttonLabel2.setStyleSheet('font: bold 25px')
         button2Box.addWidget(self.buttonLabel2)
         self.button2 = QPushButton()
@@ -87,7 +98,7 @@ class App(QWidget):
         button2Box.addWidget(self.button2)
         self.buttonLabel3 = QLabel('Calibrate')
         self.buttonLabel3.setAlignment(QtCore.Qt.AlignCenter)
-        self.buttonLabel3.setMaximumSize(200, 80)
+        self.buttonLabel3.setMaximumSize(500, 80)
         self.buttonLabel3.setStyleSheet('font: bold 25px')
         button3Box.addWidget(self.buttonLabel3)
         self.button3 = QPushButton()
@@ -99,25 +110,35 @@ class App(QWidget):
         # PWM Brightness Slider
         self.brightnessLabel = QLabel('Brightness')
         self.brightnessLabel.setStyleSheet('border: 5px solid black')
+        self.brightnessLabel.setMinimumHeight(100)
         contentVBox.addWidget(self.brightnessLabel)
         
         # Motor Rate Commands
+        rateHBox = QHBoxLayout()
         contentVBox.addLayout(rateHBox)
 
-        rateTitle = QLabel("Current Motor Positions:")
+        rateTitle = QLabel('Current Motor Positions:')
+        rateTitle.setMinimumHeight(100)
+        rateTitle.setStyleSheet('font: 30px')
         rateHBox.addWidget(rateTitle)
 
-        xRateTitle = QLabel("X:")
-        xRateTitle.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignRight)
-        rateHBox.addWidget(xRateTitle)
-        self.xRateLabel = QLabel("0")
+        self.xRateLabel = QLabel('X: 0')
+        self.xRateLabel.setMinimumHeight(100)
+        self.xRateLabel.setStyleSheet('font: 30px')
+        self.xRateLabel.setAlignment(QtCore.Qt.AlignCenter)
         rateHBox.addWidget(self.xRateLabel)
 
-        yRateTitle = QLabel("Y:")
-        yRateTitle.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignRight) 
-        rateHBox.addWidget(yRateTitle)
-        self.yRateLabel = QLabel("0")
+        self.yRateLabel = QLabel('Y: 0')
+        self.yRateLabel.setMinimumHeight(100)
+        self.yRateLabel.setStyleSheet('font: 30px')
+        self.yRateLabel.setAlignment(QtCore.Qt.AlignCenter)
         rateHBox.addWidget(self.yRateLabel)
+
+        # Command Box
+        self.commandBox = QPlainTextEdit('Initialising System...')
+        self.commandBox.setStyleSheet('border: 2px solid black; font: 10px; background-color: #C0C0C0')
+        self.commandBox.appendPlainText('Done!')
+        contentVBox.addWidget(self.commandBox)
         
 
         sys.exit(app.exec_())
